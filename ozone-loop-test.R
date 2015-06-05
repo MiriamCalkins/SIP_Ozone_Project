18a3928fc4d053535442106b99d7625a6cf8f3b1
brianhigh <bhigh@live.com>
2015-06-05 10:31:32 -0700

    Code cleanup

--- ozone-loop-test.R
+++ ozone-loop-test.R
@@ -5,9 +5,9 @@
 # Problem: Given a "site", find 4th highest ozone value for matching rows,
 #          and process many data frames with many site strings this way.
 
-# Create a function to find 4th highest "j" (oz_num) for matching "i" (site_id)
-sort_oz <- function (oz_num, site_id) {
-    with(oz_num, sort(j[i==site_id], decr=T)[4])
+# Create a function to find "nth" highest "j" value for matching "i" (id)
+highest <- function (df, id, n) {
+    with(df, sort(j[i==id], decr=T)[n])
 }
 
 # Create data frames with "i"=A-E, repeated 10 times, and random values for "j"
@@ -20,8 +20,8 @@ dflist <- list(oz1, oz2, oz3)
 
 # Loop through list of data frames with sapply and run sort function on each.
 # We use sapply() twice, but this could also be a nested pair of for-loops.
-sites <- c("A", "B", "C", "D", "E")
-res <- sapply(sites, function (y) {sapply(dflist, function(x) sort_oz(x, y))})
+ids <- c("A", "B", "C", "D", "E")
+res <- sapply(ids, function (y) {sapply(dflist, function(x) highest(x, y, 4))})
 
 # View the results
 rownames(res) <- c("oz1", "oz2", "oz3")
